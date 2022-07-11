59df158a4613a25f825dd64ddb21da69a428eab9
CesarMezaB <cmeza8599@gmail.com>
2022-07-11 16:19:41 -0700

    Add products to index.htmÃl

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log('Hi')
+const productsArray = [ 
+{
+  product: 'simple burger',
+  price:20
+},
+{
+  product:'double burger',
+  price:15  
+},
+{
+  product:'potato chips',
+  price:10  
+},
+{
+  product:'soda',
+  price:5 
+},
+];
+
+const showMenu = () => { 
+}
+   
