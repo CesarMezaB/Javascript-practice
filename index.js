d626b36f18e2e8271a1d2f09a8a7b65883885016
CesarMezaB <cmeza8599@gmail.com>
2022-07-11 18:50:03 -0700

    removing the products and leaving the show function

--- index.js
+++ index.js
@@ -1,26 +1,3 @@
-const productsArray = [ 
-{
-  name: 'simple burger',
-  price: 20,
-  code: 'S100'
-},
-{
-  name:'double burger',
-  price: 15,
-  code: 'D200'  
-},
-{
-  name:'potato chips',
-  price: 10,
-  code: 'P300'  
-},
-{
-  name:'soda',
-  price: 5,
-  code: 'A400' 
-},
-];
-
 const showMenu = () => { 
     console.log('Code - Product - Price')
     productsArray.forEach(product => console.log(`${product.code} - ${product.name} - $${product.price}`))
