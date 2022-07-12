a970eebbbde43476414e7e035d24a13530c007a0
CesarMezaB <cmeza8599@gmail.com>
2022-07-11 20:36:43 -0700

    object user and function orderProduct

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const user = {
+    name: 'pepe',
+    age: 21,
+    debt: 0,
+};
+
+const order = [];
+
 const showMenu = () => { 
-    console.log('Code - Product - Price')
+    console.log('Code - Product - Price');
     productsArray.forEach(product => console.log(`${product.code} - ${product.name} - $${product.price}`))
 }
+
+const orderProduct = (cod) =>{
+    if (!(typeof(cod) === 'string')) return 'The value is not valid'
+    if (!cod ) return 'Enter a valid code'
+    
+   const searchProduct = productsArray.find(product => product.code === cod)
+
+   if(!searchProduct) return 'The product does not exist';
+
+   order.push(searchProduct);
+   console.log('Your product has been added. Your order is:');
+   return order;
+
    
+}
