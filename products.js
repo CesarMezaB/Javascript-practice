8f480ff8ef3d95b0c3b8186b5528e361ee1b5ff1
CesarMezaB <cmeza8599@gmail.com>
2022-07-11 18:51:43 -0700

    creating products.js file and adding products

---
+++ products.js
@@ -0,0 +1,22 @@
+const productsArray = [ 
+    {
+      name: 'simple burger',
+      price: 20,
+      code: 'S100'
+    },
+    {
+      name:'double burger',
+      price: 15,
+      code: 'D200'  
+    },
+    {
+      name:'potato chips',
+      price: 10,
+      code: 'P300'  
+    },
+    {
+      name:'soda',
+      price: 5,
+      code: 'A400' 
+    },
+    ];
